6db6a31dc0c719878a6e4a60ae35d8dbf2e59e67
Anibal Sólon <anibalsolon@gmail.com>
2026-04-15 14:00:32 -0500

    enh: mock worker and matchmedia, rename to module typescript (.mts) file

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,55 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+type MockWorkerRequest = {
+  id?: string;
+  type?: string;
+};
+
+class MockWorker {
+  onmessage: ((event: MessageEvent) => void) | null = null;
+  onerror: ((event: ErrorEvent) => void) | null = null;
+
+  postMessage(message: MockWorkerRequest): void {
+    queueMicrotask(() => {
+      this.onmessage?.({
+        data: {
+          id: message.id,
+          type: 'success',
+          payload: message.type === 'initialize' ? undefined : null,
+        },
+      } as MessageEvent);
+    });
+  }
+
+  terminate(): void {}
+  addEventListener(): void {}
+  removeEventListener(): void {}
+  dispatchEvent(): boolean {
+    return true;
+  }
+}
+
+if (!globalThis.Worker) {
+  Object.defineProperty(globalThis, 'Worker', {
+    writable: true,
+    value: MockWorker,
+  });
+}
+
+if (!window.matchMedia) {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: (query: string): MediaQueryList => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: () => {},
+      removeListener: () => {},
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      dispatchEvent: () => true,
+    }),
+  });
+}
